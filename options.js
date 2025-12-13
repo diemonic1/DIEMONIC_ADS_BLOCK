48b166fb0077c9f2b7fc44a176adeed12dfded58
diemonic1 <diemonic1@gmail.com>
2025-12-13 18:44:59 +0300

    update settings

--- options.js
+++ options.js
@@ -1,17 +1,45 @@
-chrome.storage.local.get(['DIEMONIC_ADS_BLOCK_last_time_update_configs'], (result) => {
-    const el = document.getElementById('DIEMONIC_ADS_BLOCK_latUpdateTime');
-    el.innerHTML = result.DIEMONIC_ADS_BLOCK_last_time_update_configs;
-    console.log(result.DIEMONIC_ADS_BLOCK_last_time_update_configs);
-});
-
-chrome.storage.local.get(['DIEMONIC_ADS_BLOCK_elementsToDelete', 'DIEMONIC_ADS_BLOCK_elementsToCheck', 'DIEMONIC_ADS_BLOCK_banWords', 'DIEMONIC_ADS_BLOCK_stopWords'], (result) => {
-    document.getElementById('DIEMONIC_ADS_BLOCK_elementsToDelete').innerHTML += result.DIEMONIC_ADS_BLOCK_elementsToDelete;
-    document.getElementById('DIEMONIC_ADS_BLOCK_elementsToCheck').innerHTML += result.DIEMONIC_ADS_BLOCK_elementsToCheck;
-    document.getElementById('DIEMONIC_ADS_BLOCK_banWords').innerHTML += result.DIEMONIC_ADS_BLOCK_banWords;
-    document.getElementById('DIEMONIC_ADS_BLOCK_stopWords').innerHTML += result.DIEMONIC_ADS_BLOCK_stopWords;
-});
-
 document.addEventListener('DOMContentLoaded', () => {
+    chrome.storage.local.get(['DIEMONIC_ADS_BLOCK_last_time_update_configs'], (result) => {
+        const el = document.getElementById('DIEMONIC_ADS_BLOCK_latUpdateTime');
+        el.innerHTML = result.DIEMONIC_ADS_BLOCK_last_time_update_configs;
+        console.log(result.DIEMONIC_ADS_BLOCK_last_time_update_configs);
+    });
+
+    chrome.storage.local.get(['DIEMONIC_ADS_BLOCK_elementsToDelete', 'DIEMONIC_ADS_BLOCK_elementsToCheck', 'DIEMONIC_ADS_BLOCK_banWords', 'DIEMONIC_ADS_BLOCK_stopWords'], (result) => {
+        let s = "";
+
+        let mass = result.DIEMONIC_ADS_BLOCK_elementsToDelete.split(',');
+        for (let index = 0; index < mass.length; index++) {
+            const element = mass[index];
+            s += ("<p>" + (index + 1) + ") " + element + "</p>");
+        }
+        document.getElementById('DIEMONIC_ADS_BLOCK_elementsToDelete').innerHTML = s;
+
+        s = "";
+        mass = result.DIEMONIC_ADS_BLOCK_elementsToCheck.split(',');
+        for (let index = 0; index < mass.length; index++) {
+            const element = mass[index];
+            s += ("<p>" + (index + 1) + ") " + element + "</p>");
+        }
+        document.getElementById('DIEMONIC_ADS_BLOCK_elementsToCheck').innerHTML = s;
+
+        s = "";
+        mass = result.DIEMONIC_ADS_BLOCK_banWords.split(',');
+        for (let index = 0; index < mass.length; index++) {
+            const element = mass[index];
+            s += ("<p>" + (index + 1) + ") " + element + "</p>");
+        }
+        document.getElementById('DIEMONIC_ADS_BLOCK_banWords').innerHTML = s;
+
+        s = "";
+        mass = result.DIEMONIC_ADS_BLOCK_stopWords.split(',');
+        for (let index = 0; index < mass.length; index++) {
+            const element = mass[index];
+            s += ("<p>" + (index + 1) + ") " + element + "</p>");
+        }
+        document.getElementById('DIEMONIC_ADS_BLOCK_stopWords').innerHTML = s;
+    });
+
     const btn = document.getElementById('DIEMONIC_ADS_BLOCK_resetBtn');
     btn.addEventListener('click', () => {
         chrome.storage.local.clear();
